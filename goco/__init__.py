7b7a3b04ed0cd3cd6cbea290f8d65c7934d2a638
Khaled ElMorshedy <31712173+elmoiv@users.noreply.github.com>
2019-11-30 15:55:01 +0200

    Update __init__.py

--- goco/__init__.py
+++ goco/__init__.py
@@ -1,7 +1,5 @@
 from goco.goco import Goco
 
-"""A library that provides easy connection to Google Services API"""
-
 __author__ = 'Khaled H. El-Morshedy'
 __url__ = 'https://github.com/elmoiv/goco'
 __description__ = 'A simple authenticator for google apis'
