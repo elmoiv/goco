98510a9797c9363d3baae1c7cd6faf6b59e42755
Khaled ElMorshedy <31712173+elmoiv@users.noreply.github.com>
2019-11-30 16:13:34 +0200

    Update goco.py

--- goco/goco.py
+++ goco/goco.py
@@ -19,24 +19,20 @@ class Goco:
     def __init__(self, client_secret_path):
         self.csecp = client_secret_path
         self.csrgp = 'credentials.storage'
-
         if not os.path.exists(self.csecp):
             raise FileNotFoundError('client_secret.json')
 
     def authorize_credentials(self, scope):
         STORAGE = Storage(self.csrgp)
         credentials = STORAGE.get()
-
         if credentials is None or credentials.invalid:
             flow = flow_from_clientsecrets(self.csecp, scope='https://www.googleapis.com/auth/' + scope)
             http = httplib2.Http()
             credentials = run_flow(flow, STORAGE, http=http)
-
         return credentials
 
     def refresh_access_token(self):
         data = json.loads(open(self.csecp).read())['installed']
-
         client_id = data['client_id']
         client_secret = data['client_secret']
         refresh_token = json.loads(open(self.csrgp).read())['refresh_token']
@@ -44,36 +40,31 @@ class Goco:
         # Getting new "access_token"
         request = requests.post(
             'https://accounts.google.com/o/oauth2/token',
-
             data=urlencode({
                 'grant_type':    'refresh_token',
                 'client_id':     client_id,
                 'client_secret': client_secret,
                 'refresh_token': refresh_token
                 }),
-
             headers={
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Accept':       'application/json'
                 }
             )
-
         response = json.loads(request.content)
         return response['access_token']
 
     def update_credentials(self, access_token):
         creds = json.loads(open(self.csrgp).read())
-
         date = str(datetime.datetime.now()).split(' ')
 
         # Token expiry is in RFC 3339 date-time format
         # YYYY-MM-DDTHH:MM:SSZ
         token_expiry = '{}T{}Z'.format(date[0], date[1][:8])
-
         creds["token_expiry"] = token_expiry
         creds["access_token"] = access_token
         creds["token_response"]["access_token"] = access_token
-
+        
         with open(self.csrgp, 'w') as creds_new:
             json.dump(creds, creds_new)
         
@@ -92,7 +83,6 @@ class Goco:
         
         # Connecting to chosen google service
         http_auth = cerds.authorize(httplib2.Http())
-
         service = discovery.build(
             service_name,
             version,
